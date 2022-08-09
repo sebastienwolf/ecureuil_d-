9a1e7984a145e49ee44b612ea4e9aee949c1ec38
sebastienwolf <s.mouret.pro@gmail.com>
2022-08-09 19:47:52 +0200

    end algo result

---
+++ README.md
@@ -0,0 +1,6 @@
+Création en cours:
+
+Application pour des parties de JDR en ligne basées sur l'univers "Les écureuils attaquent!".
+
+Dés cachés dans des noix. Il faudra piocher un dé quand le MJ le demandera. Si un dé se dévoile, on pourra lancer le dé.
+Ce sera une application par instance pour être plusieurs joueurs sur une même session.
--- script.js
+++ script.js
@@ -9,39 +9,55 @@ const plateau = document.getElementById("jeu");
 // 1 plein
 const arrayNoix = [20, 12, 8, 8, 6, 6, 6, 4, 4, "vide", "vide", "noix"];
 
-document.getElementById("reset").addEventListener("click", (event) => {
-  event.preventDefault();
-  let copy = Object.values(arrayNoix);
+// document.getElementById("reset").addEventListener("click", (event) => {
+//   event.preventDefault();
+let copy = Object.values(arrayNoix);
 
-  for (let index = 0; index < arrayNoix.length; index++) {
-    let key = Math.floor(Math.random() * copy.length);
-    let dee = document.createElement("article");
-    let description = document.createElement("h2");
-    description.innerText = "Dé " + copy[key];
+for (let index = 0; index < arrayNoix.length; index++) {
+  let key = Math.floor(Math.random() * copy.length);
+  let dee = document.createElement("article");
+  dee.id = "select" + index;
 
+  let description = document.createElement("h2");
+  description.innerText = "Dé " + copy[key];
+  dee.appendChild(description);
+
+  if (copy[key] !== "vide" && copy[key] !== "noix") {
     let lancerDeDee = document.createElement("button");
     lancerDeDee.value = copy[key];
     lancerDeDee.innerText = "Lancer";
     lancerDeDee.className = "throw";
-
-    dee.appendChild(description);
+    lancerDeDee.dataset.id = copy[key];
+    lancerDeDee.dataset.select = index;
     dee.appendChild(lancerDeDee);
-    plateau.appendChild(dee);
-
-    let noix = document.createElement("img");
-    noix.src = "./image/noix.png";
-    noix.alt = "veuillez recharger la page";
-    plateau.appendChild(noix);
-    copy.splice(key, 1);
   }
-});
 
-const buttonDé = document.getElementsByClassName("throw");
+  plateau.appendChild(dee);
 
-for (let buttonChoice of buttonDé) {
+  let noix = document.createElement("img");
+  noix.src = "./image/noix.png";
+  noix.alt = "veuillez recharger la page";
+  plateau.appendChild(noix);
+  copy.splice(key, 1);
+}
+let buttonDé = document.getElementsByClassName("throw");
+
+//});
+
+let buttonChoice;
+for (buttonChoice of buttonDé) {
   buttonChoice.addEventListener("click", (e) => {
-    e.preventDefault();
-    debugger;
-    let a = 1;
+    let nombreDeFace = e.target.dataset.id;
+    let position = e.target.dataset.select;
+
+    if (nombreDeFace !== "") {
+      debugger;
+      let result = Math.floor(Math.random() * (nombreDeFace - 1)) + 1;
+      let createResult = document.createElement("p");
+      let addResult = document.getElementById("select" + position);
+
+      createResult.innerText = "le résultat : " + result;
+      addResult.appendChild(createResult);
+    }
   });
 }
